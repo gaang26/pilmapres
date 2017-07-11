9f750a35f5b2470ed51268ae4810243741f03276
Arif Rahman Hakim (Cethol) <klicethol@gmail.com>
2017-07-11 20:45:45 +0700

    streaming

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -6,28 +6,38 @@ $this->pageTitle='Beranda';
 
 <?php $this->renderPartial('home/slider'); ?>
 
- <div class="row-fluid">
+<div class="row-fluid">
+    <div class="span8 offset2 margin-top-20 text-center" style="border-bottom:1px dashed #DDD; padding-bottom: 25px;">
+        <h4 style="text-transform: uppercase">Streaming Penganugerahan Mahasiswa Berprestasi</h4>
+        <div class="margin-top-10">
+            <?php echo CHtml::link('BUKA LINK STREAMING','http://youtu.be/xWj_OIpHqBk',array('class'=>'btn btn-lg red','target'=>'_blank')); ?>
+            <p class="margin-top-10" style="font-size: 0.9em">Streaming dimulai pada tanggal 13 Juli 2017 Pukul 19:00 WIB</p>
+        </div>
+    </div>
+</div>
+
+ <!--<div class="row-fluid">
     <div class="span8 offset2 margin-top-20">
-        <?php if(Jadwal::isPengumumanOpen()): ?>
+        <?php /*if(Jadwal::isPengumumanOpen()): */?>
             <div class="well text-center">
-                <h4>PENGUMUMAN FINALIS MAWAPRES NASIONAL <?php echo Yii::app()->params['tahun'];?></h4>
+                <h4>PENGUMUMAN FINALIS MAWAPRES NASIONAL <?php /*echo Yii::app()->params['tahun'];*/?></h4>
                 <div class="">
                     Peserta yang lolos tahap selanjutnya dapat dilihat pada alamat dibawah ini.
                 </div>
                 <div class="margin-top-20">
-                    <?php echo CHtml::link('LIHAT FINALIS MAWAPRES NASIONAL '.Yii::app()->params['tahun'],array('finalis/index'),array(
+                    <?php /*echo CHtml::link('LIHAT FINALIS MAWAPRES NASIONAL '.Yii::app()->params['tahun'],array('finalis/index'),array(
                         'class'=>'btn red btn-lg'
-                    )); ?>
+                    )); */?>
                 </div>
             </div>
-        <?php endif; ?>
+        <?php /*endif; */?>
     </div>
-</div>
+</div>-->
 
-<?php $this->renderPartial('home/finalis',array(
+<?php /*$this->renderPartial('home/finalis',array(
     'sarjana'=>Peserta::getFinalis(Peserta::SARJANA),
     'diploma'=>Peserta::getFinalis(Peserta::DIPLOMA)
-)); ?>
+));*/ ?>
 
 <?php /*$this->renderPartial('home/juara',array(
     'sarjana'=>Peserta::getJuara(Peserta::SARJANA),
